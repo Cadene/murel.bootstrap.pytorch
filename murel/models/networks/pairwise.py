7c9eaebfa6b0fe2565d97dac01001ea9e6ddae7b
Remi Cadene <remi.cadene@icloud.com>
2019-08-08 14:11:02 -0700

    Fix pairwise (fast indexing, remove no_grad)

--- murel/models/networks/pairwise.py
+++ murel/models/networks/pairwise.py
@@ -1,20 +1,8 @@
-import itertools
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import block
 
-def make_pairs_ids(n_regions, bsize):
-    pairs_ids = []
-    for batch_id in range(bsize):
-        pairs_id = torch.tensor([
-            (batch_id,i,j) for i,j in \
-            itertools.product(range(n_regions),repeat=2)],
-            requires_grad=False,
-            dtype=torch.long)
-        pairs_ids.append(pairs_id)
-    out = torch.cat(pairs_ids).contiguous()
-    return out
 
 class Pairwise(nn.Module):
 
@@ -35,40 +23,38 @@ class Pairwise(nn.Module):
             self.f_feat_module = block.factory_fusion(self.fusion_feat)
         #
         self.buffer = None
-        self.pairs_ids = None
-        self.pairs_ids_n_regions = None
-        self.pairs_ids_bsize = None
-
-    def set_pairs_ids(self, n_regions, bsize, device):
-        self.pairs_ids = make_pairs_ids(n_regions, bsize)
-        self.pairs_ids = self.pairs_ids.to(device=device, non_blocking=True)
-        self.pairs_ids_n_regions = n_regions
-        self.pairs_ids_bsize = bsize
 
     def set_buffer(self):
         self.buffer = {}
 
     def forward(self, mm, coords=None):
         bsize = mm.shape[0]
-        n_regions = mm.shape[1]
-
-        if self.pairs_ids is None \
-           or self.pairs_ids_n_regions != n_regions \
-           or self.pairs_ids_bsize != bsize:
-            self.set_pairs_ids(n_regions, bsize, device=mm.device)
+        nregion = mm.shape[1]
 
         Rij = 0
         if self.fusion_coord:
             assert coords is not None
-            pair_coords = coords[self.pairs_ids[:,0][:,None], self.pairs_ids[:,1:]]
-            pair_coords.detach_() # REALLY IMPORTANT
-            Rij += self.f_coord_module([pair_coords[:,0,:], pair_coords[:,1,:]])
+            coords_l = coords[:,:,None,:]
+            coords_l = coords_l.expand(bsize,nregion,nregion,coords.shape[-1])
+            coords_l = coords_l.contiguous()
+            coords_l = coords_l.view(bsize*nregion*nregion,coords.shape[-1])
+            coords_r = coords[:,None,:,:]
+            coords_r = coords_r.expand(bsize,nregion,nregion,coords.shape[-1])
+            coords_r = coords_r.contiguous()
+            coords_r = coords_r.view(bsize*nregion*nregion,coords.shape[-1])
+            Rij += self.f_coord_module([coords_l, coords_r])
         if self.fusion_feat:
-            pair_mm = mm[self.pairs_ids[:,0][:,None], self.pairs_ids[:,1:]]
-            pair_mm.detach_() # REALLY IMPORTANT
-            Rij += self.f_feat_module([pair_mm[:,0,:], pair_mm[:,1,:]])
+            mm_l = mm[:,:,None,:]
+            mm_l = mm_l.expand(bsize,nregion,nregion,mm.shape[-1])
+            mm_l = mm_l.contiguous()
+            mm_l = mm_l.view(bsize*nregion*nregion,mm.shape[-1])
+            mm_r = mm[:,None,:,:]
+            mm_r = mm_r.expand(bsize,nregion,nregion,mm.shape[-1])
+            mm_r = mm_r.contiguous()
+            mm_r = mm_r.view(bsize*nregion*nregion,mm.shape[-1])
+            Rij += self.f_feat_module([mm_l, mm_r])
 
-        Rij = Rij.view(bsize, n_regions, n_regions, -1)
+        Rij = Rij.view(bsize,nregion,nregion,-1)
 
         if self.agg['type'] == 'max':
             mm_new, argmax = Rij.max(2)
